0a11bf28b4cdd6521602765ecc4725bf8aadc35f
sherchryst <sherchryst@icloud.com>
2021-04-28 16:53:42 +0200

    update

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/29 03:39:23 by sgah              #+#    #+#             */
+/*   Updated: 2019/10/25 19:52:33 by sgah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct			s_file
+{
+	char				*buf;
+	int					fd;
+	struct s_file		*next;
+}						t_file;
+
+size_t					ft_strlen(const char *s);
+char					*ft_strcpy(char *dst, const char *src);
+char					*ft_strjoin_free(char **s1, char *s2);
+char					*ft_strdup(const char *s1);
+int						get_next_line(int fd, char **line);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/17 02:12:35 by sgah              #+#    #+#             */
+/*   Updated: 2019/10/25 19:53:17 by sgah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t		ft_strlen(const char *s)
+{
+	size_t len;
+
+	len = 0;
+	while (s && s[len])
+		len++;
+	return (len);
+}
+
+char		*ft_strcpy(char *dst, const char *src)
+{
+	int i;
+
+	i = 0;
+	while (src && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char		*ft_strdup(const char *s1)
+{
+	char		*dup;
+	size_t		i;
+
+	i = 0;
+	if (!(dup = (char *)malloc(sizeof(char *) * ft_strlen(s1) + 1)))
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		dup[i] = ((char *)s1)[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char		*ft_strjoin_free(char **s1, char *s2)
+{
+	char	*r;
+	char	*c1;
+	char	*c2;
+	size_t	i;
+
+	if (!(r = (char *)malloc(ft_strlen(*s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	c1 = *s1;
+	c2 = s2;
+	i = 0;
+	while (*c1)
+		r[i++] = *(c1++);
+	while (*c2)
+		r[i++] = *(c2++);
+	r[i] = '\0';
+	free(*s1);
+	return (r);
+}
